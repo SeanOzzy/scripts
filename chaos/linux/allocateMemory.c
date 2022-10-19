0e57edfe65d1f00f01fc5c4033d20f457e98e7a0
SeanOzzy <114792501+SeanOzzy@users.noreply.github.com>
2022-10-19 15:40:05 +1100

    Create allocateMemory.c

---
+++ chaos/linux/allocateMemory.c
@@ -0,0 +1,18 @@
+/* 
+* This code will consume memory and should not be used in production.
+* 
+* gcc allocateMemory.c -o allocatememory
+*
+*/
+
+#include <stdlib.h>
+    #include <stdio.h>
+
+    int main() {
+        int i = 0;
+        char* buffer = (char*) calloc(3000000000,1);
+        printf("Done allocating\n");
+        scanf("%d",&i);
+        free(buffer);
+        return 0;
+    }
